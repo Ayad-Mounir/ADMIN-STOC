9a27d7b6d70688871a4155f518807cf87e03c5ca
AYAD_MOUNIR <contact.ayad.mounir@gmail.com>
2026-05-17 16:27:49 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stoc-admin-v1';
+const CACHE = 'stoc-admin-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
